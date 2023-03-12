eb1413c5b7ae998ba3bdda2e90cc4efb42a015ed
helmy <work4real123@gmail.com>
2023-03-12 18:35:27 +0200

    first commit

---
+++ Kompas.js
@@ -0,0 +1,14 @@
+// let header = document.getElementById('hd');
+// let nav = document.querySelector('.nav');
+// window.onscroll = function(){
+//     if(scrollY > 660){
+//         header.style.backgroundColor = "#fafafa";
+//         nav.style.Color = "black";
+//     }
+//     else{
+//         header.style.backgroundColor = "transparent";
+//         nav.style.Color = "#fff";
+        
+//     }
+    
+// }
